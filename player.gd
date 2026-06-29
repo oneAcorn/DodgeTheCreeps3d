92f8e10e53c8631716e920307abb920e4634c417
sufei <sufei@weichengya.cn>
2026-06-29 16:35:28 +0800

    game logic

---
+++ player.gd
@@ -0,0 +1,77 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+@export var jump_impulse = 20
+# Vertical impulse applied to the character upon bouncing over a mob in
+# meters per second.
+@export var bounce_impulse = 16
+
+var target_velocity = Vector3.ZERO
+
+# Emitted when the player was hit by a mob.
+signal hit
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_up"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.z += 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+
+	# ground velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+
+	move_and_slide()
+
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If there are duplicate collisions with a mob in a single frame
+		# the mob will be deleted after the first collision, and a second call to
+		# get_collider will return null, leading to a null pointer when calling
+		# collision.get_collider().is_in_group("mob").
+		# This block of code prevents processing duplicate collisions.
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if (collision.get_collider() as Node).is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.6:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				break
+
+
+func die():
+	hit.emit()
+	queue_free()
+
+
+func _on_mob_detector_body_entered(body: Node3D) -> void:
+	die()
